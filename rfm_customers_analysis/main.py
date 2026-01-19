c49231ebed73394226bacf7ff2143d0161d5e9e3
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-19 11:36:35 -0300

    Create main.py

---
+++ rfm_customers_analysis/main.py
@@ -0,0 +1,17 @@
+"""
+RFM Clients Analysis
+A tool that transforms raw clients data into a ready-to-use executive RFM report
+"""
+
+from scheduler import Scheduler
+
+scheduler = Scheduler(input_file="input/clients_sample.csv")
+
+# Single run
+scheduler.run_pipeline()
+
+# Run every day at 09:00
+# scheduler.schedule_daily("09:00")
+
+# Or run every 3 days
+# scheduler.schedule_every_n_days(3)
